7f0f6adc434e84404a590a25e4935432d3420516
Joonas Loppi <joonas+code@joonas.fi>
2022-01-16 12:14:49 +0200

    update links

--- cmd/shorturl/linkdb.go
+++ cmd/shorturl/linkdb.go
@@ -3,5 +3,7 @@ package main
 // hardcoded link DB for now
 
 var linkdb = map[string]string{
-	"turbobob-latest-linux-amd64": "https://github.com/function61/turbobob/releases/download/20200910_1241_90ec91c9/bob_linux-amd64",
+	"turbobob-latest-stable-linux-amd64": "https://function61.com/app-dl/api/github.com/function61/turbobob/latest_releases_asset/bob_linux-amd64",
+	// backwards compat
+	"turbobob-latest-linux-amd64": "https://function61.com/app-dl/api/github.com/function61/turbobob/latest_releases_asset/bob_linux-amd64",
 }
